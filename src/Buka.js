59d87c78665e0b82e885cc619aacbb044f8b40f2
SparkySparkyBoom <64778205+SparkySparkyBoom@users.noreply.github.com>
2020-09-05 21:27:09 +0700

    new
    tambahan buka

---
+++ src/Buka.js
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react';
+import Header from './views/Header'
+import {TableContext} from './views/Context'
+
+function Buka() {
+    return(        
+        <div >
+        <Header />
+        
+         </div>
+        
+    )
+}
+export default Buka;

946f691e03edd8b4f971a1a077f3d23e6d0cf98d
SparkySparkyBoom <64778205+SparkySparkyBoom@users.noreply.github.com>
2020-09-05 21:32:35 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom'
-import {Home} from './views'
-import {Form,Button} from 'react-bootstrap'
-import Login from './views/Login'
-import Tbl from './views/Table'
 import Header from './views/Header'
-function App() {
-  const[check,setCheck]=useState(true);
-  function ganti() {
-    setCheck(false)
-  }
+import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom'
+import Home from './views/Home'
+import TambahTable from './views/TambahTable'
+import Login from './views/Login'
+import {TableProvider} from './views/Context'
+function App() {  
   
-  return (    
+  return (
+     <TableProvider>
+       <Router>
     <div >
-      <Home/>
-      {/* {check?<Login onClick={ganti}/>:<Home/>}
-      <Table data={dataset}/> */}
-    </div>     
+    <Header />
+    
+    </div>
+    <Switch>
+      <Route path="/" component={Login} exact/>
+      <Route path="/home" component={Home} />
+      <Route path="/tambahtable" component={TambahTable}/>
+    </Switch>                   
+    </Router>
+    </TableProvider> 
+    
   );
 }
 export default App;
